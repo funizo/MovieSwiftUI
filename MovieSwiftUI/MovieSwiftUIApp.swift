244fd44011481345136223cba3976c29c75a3948
funizo <k4160621@naver.com>
2022-10-10 12:10:28 +0900

    Initial Commit

---
+++ MovieSwiftUI/MovieSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieSwiftUIApp.swift
+//  MovieSwiftUI
+//
+//  Created by 김세준 on 2022/10/10.
+//
+
+import SwiftUI
+
+@main
+struct MovieSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MovieSwiftUIUITests/MovieSwiftUIUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  MovieSwiftUIUITestsLaunchTests.swift
+//  MovieSwiftUIUITests
+//
+//  Created by 김세준 on 2022/10/10.
+//
+
+import XCTest
+
+class MovieSwiftUIUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
